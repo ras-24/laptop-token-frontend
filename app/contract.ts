25fb1e755797000ba20800a49efd5847d2e993a4
Ras <siregar.riyando@gmail.com>
2024-11-24 11:37:49 +0800

    Adding environment variable

--- app/contract.ts
+++ app/contract.ts
@@ -3,10 +3,10 @@ import ABI from "./ABI.json";
 
 let web3: Web3 | undefined;
 let contract: any;
+const contractAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS;
 
 if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
   web3 = new Web3(window.ethereum);
-  const contractAddress = "YOUR_CONTRACT_ADDRESS"; // Change with your Contract Address
   contract = new (web3 as any).eth.Contract(ABI, contractAddress);
 } else {
   console.log(
